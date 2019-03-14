c76b604a47a4ebcfa61580c0216cb6f51c2faa59
Elijah Sawyers <elijahsawyers@gmail.com>
2019-03-14 13:57:28 -0500

    First commit

---
+++ PDFConverter.py
@@ -0,0 +1,32 @@
+'''
+    Created by Elijah Sawyers on 03/07/19.
+
+    Abstract:
+    This file contains the functionality for converting a pdf to png and
+    doing coordinates conversions between the two.
+'''
+
+import sys
+from wand.image import Image
+
+'''This class can convert pdf files into png files.'''
+class PDFConverter: 
+
+    # Initialize the PDF converter.
+    def __init__(self, pdfFilePath):
+        self.pdfFile = Image(filename=pdfFilePath)
+
+    # Converts a pdf file into a png file.
+    def convertToPng(self, newFilePath):
+        with self.pdfFile as pdfToConvert:
+            # Convert to png.
+            with pdfToConvert.convert("png") as convertedPdf:
+                convertedPdf.save(filename=newFilePath)
+
+# Run the script.
+if __name__ == "__main__":
+    if (len(sys.argv) != 3):
+        print("Usage: python PDFConverter.py PDFPath PNGName")
+    else:
+        pdfConverter = PDFConverter(sys.argv[1])
+        pdfConverter.convertToPng(sys.argv[2])
